a206d408aa2bbaef8d6e00acd9e563e0a39c0c37
sarahduv <sarah.duvdevani@gmail.com>
2019-05-20 22:32:05 -0700

    wheel is looking awesome, monday night

--- js/game.js
+++ js/game.js
@@ -1,22 +1,29 @@
 var svgns = 'http://www.w3.org/2000/svg';
+var svgxlink = 'http://www.w3.org/1999/xlink'
 var sliceEnds = [];
 var numSlices = 12; // How many slices do we want
 var slicesContainer = document.getElementById('slicesContainer');
 var sliceSize = 300 / numSlices;
+var markerRect = document.getElementById('markerDiv').getBoundingClientRect();
+var wrapperEl = document.getElementById('wrapper');
+var unicornImageUrl = "http://cdn.pixabay.com/photo/2016/03/29/22/12/unicorn-1289544_960_720.png";
+var catImageUrl = "https://cdn.pixabay.com/photo/2016/04/06/17/42/silhouette-1312357_960_720.png";
 
 // Get the wheel disc that we are rotating
 // We start at angle 0, and every 10ms add 2 degrees
 var disc = document.getElementById('wheelDisc');
 var discAngle = 0;
 var updateMs = 10;
-var rotationSpeed = getRandomIntInclusive(5, 80);
-var slowDownFactor = .99;
+var rotationSpeed = 0;
+var slowDownFactor = .992;
 
 // Button for event listener
 var spinButton = document.getElementById('spinButton');
 
 function SliceEnd(num) {
   this.id = 'sliceEnd_' + num;
+  this.isCat = false;
+  this.cashValue = 100;
   
   // eslint-disable-next-line no-trailing-spaces
   // Every time you continue from where you left off, 
@@ -26,10 +33,33 @@ function SliceEnd(num) {
   this.offset = num * sliceSize;
 
   // Store the svg object in case we want to use it later
-  this.object = this.createSvgSlice(num);
+  var svgForSlice = this.createSvgSlice(num);
 
   // Add the svg element to the DOM
-  slicesContainer.appendChild(this.object);
+  slicesContainer.appendChild(svgForSlice);
+}
+ 
+SliceEnd.prototype.turnIntoCat = function(doAnimation) {
+  var iconId = this.id + "_icon";
+  var iconEl = document.getElementById(iconId);
+  if (!this.isCat) {
+    this.isCat = true;
+    this.cashValue = 0;
+
+    if (doAnimation) {
+      // Do a pow animation where the image is right now
+      var iconRect = iconEl.getBoundingClientRect();
+      var wrapperRect = wrapperEl.getBoundingClientRect();
+      doPow(iconRect.left - wrapperRect.left, iconRect.top - wrapperRect.top);
+      
+      // When the pow is biggest, change the image
+      setTimeout(function() {
+        iconEl.setAttributeNS(svgxlink, 'href', catImageUrl);
+      }, 400);
+    } else {
+      iconEl.setAttributeNS(svgxlink, 'href', catImageUrl);
+    }
+  }
 }
 
 SliceEnd.prototype.createSvgSlice = function(num) {
@@ -41,29 +71,47 @@ SliceEnd.prototype.createSvgSlice = function(num) {
   sliceEnd.setAttribute('class', 'sliceEnd');
   
   // First line to
-  var x1 = 250 + 230 * Math.cos(Math.PI * (-90 + this.offset) / 150);
-  var y1 = 250 + 230 * Math.sin(Math.PI * (-90 + this.offset) / 150);
+  var radius = 230;
+  var x1 = 250 + radius * Math.cos(Math.PI * (-90 + this.offset) / 150);
+  var y1 = 250 + radius * Math.sin(Math.PI * (-90 + this.offset) / 150);
   // Then arc to
-  var x2 = 250 + 230 * Math.cos(Math.PI * (-90 + sliceSize + this.offset) / 150);
-  var y2 = 250 + 230 * Math.sin(Math.PI * (-90 + sliceSize + this.offset) / 150);
+  var x2 = 250 + radius * Math.cos(Math.PI * (-90 + sliceSize + this.offset) / 150);
+  var y2 = 250 + radius * Math.sin(Math.PI * (-90 + sliceSize + this.offset) / 150);
+
   sliceEnd.setAttribute(
     'd', 
     'M 250 250 ' + // M = set current position 250,250, (center)
       'L ' + x1 + ' ' + y1 + ' ' + // L = line to, x1 y1, probably left side of slice
-      'A 230 230 0 0 1 ' + x2 + ' ' + y2 + // A = Arc to (outer part of the circle)
+      'A ' + radius + ' ' + radius + ' 0 0 1 ' + x2 + ' ' + y2 + // A = Arc to (outer part of the circle)
       '  Z'); // Z = auto close shape and fill it
 
-  // // For the first slice only, make it green
-  // if (num == 0) {
-  //   sliceEnd.setAttribute('fill', 'rgb(0,204,0)');
-  // // Otherwise if it's even then red
-  // } else if (num % 2 == 0) {
-  //   sliceEnd.setAttribute('fill', 'rgb(204,0,0)');
-  // // Otherwise it's black
-  // } else {
-  //   sliceEnd.setAttribute('fill', 'rgb(0,0,0)');
-  // }
+  // The middle between the two sides of the arcs
+  var xAvgPt1Pt2 = ((x1 + x2) / 2);
+  var yAvgPt1Pt2 = ((y1 + y2) / 2);
 
+  // "Average" the current location with the center to bring it 
+  // closer to the center
+  var xIcon = xAvgPt1Pt2 * 0.85 + 250 * 0.15;
+  var yIcon = yAvgPt1Pt2 * 0.85 + 250 * 0.15;
+
+  // We have numSlices slices of 360 degrees, and this is
+  // icon number num (out of the numSlices)
+  var iconAngle = ((360 / numSlices) * num);
+
+  // Create the slice icon 
+  var sliceIcon = document.createElementNS(svgns, 'image');
+  sliceIcon.setAttributeNS(svgxlink, 'href', unicornImageUrl);
+  sliceIcon.setAttribute('id', this.id + "_icon");
+  sliceIcon.setAttribute('class', 'sliceIcon');
+  sliceIcon.setAttribute('width', '50');
+  sliceIcon.setAttribute('height', '50');
+  sliceIcon.setAttribute('x', xIcon - 25);
+  sliceIcon.setAttribute('y', yIcon - 25);
+  sliceIcon.setAttribute(
+    'transform', 
+    'rotate(' + iconAngle + ' ' + xIcon + ' ' + yIcon + ')'
+  );
+  
   // The frequency is the rate at which the color hue changes
   var frequency = .4;
   var red = Math.sin(frequency * num + 0) * 127 + 128;
@@ -71,21 +119,118 @@ SliceEnd.prototype.createSvgSlice = function(num) {
   var blue = Math.sin(frequency * num + 4) * 127 + 128;
   sliceEnd.setAttribute('fill', 'rgb('+red+','+green+','+blue+')');
 
+  var referencePoint = document.createElementNS(svgns, 'rect');
+  referencePoint.setAttribute('id', this.id + "_ref");
+  referencePoint.setAttribute('class', 'sliceRefPoint');
+  referencePoint.setAttribute('width', '1');
+  referencePoint.setAttribute('height', '1');
+  referencePoint.setAttribute('x', xAvgPt1Pt2);
+  referencePoint.setAttribute('y', yAvgPt1Pt2);
+
   g.appendChild(sliceEnd);
+  g.appendChild(sliceIcon);
+  g.appendChild(referencePoint);
+  this.sliceIcon = sliceIcon;
+  this.slicePath = sliceEnd;
+  this.referencePoint = referencePoint;
   return g;
 };
-
 
 function onTimerTick() {
   discAngle += rotationSpeed;
   rotationSpeed *= slowDownFactor;
   disc.setAttribute('transform', 'rotate(' + discAngle + ' 250 250)');
 
-  if (rotationSpeed < 0.01) {
+  // this is to verify our idea of selecting the right thing works
+  // rightmost = getRightmostSlice();
+  // for (var i = 0; i < numSlices; i++) {
+  //   sliceEnds[i].slicePath.setAttribute("fill", "white");
+  // }
+  // rightmost.slicePath.setAttribute("fill", "blue");
+
+  if (rotationSpeed > 0 && rotationSpeed < 0.01) {
     rotationSpeed = 0;
+    var winnerSlice = getRightmostSlice();
+    console.log('You just won $' + winnerSlice.cashValue);
   }
+}
 
+function handleSpinButton(event){
+// Set the timer that will update every X ms
+  // event.preventDefault();
+  console.log('this is the beginning of the handler');
+  rotationSpeed = getRandomIntInclusive(50, 150);
+}
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+function getRightmostSlice() {
+  var closestSlice = null;
+  var closestDistance = 999999;
+
+  for (var i = 0; i < numSlices; i++) {
+    // Get the x,y of the reference point of the next shape
+    var rect = sliceEnds[i].referencePoint.getClientRects()[0];
+    var distance = Math.sqrt(
+      ((rect.x - markerRect.x) * (rect.x - markerRect.x)) +
+      ((rect.y - markerRect.y) * (rect.y - markerRect.y))
+    );
+
+    // If that distance between that reference point and the marker is smaller
+    // than all the distances we've had so far 
+    if (distance < closestDistance) {
+      // Replace the smallest so far with this one
+      closestSlice = sliceEnds[i];
+      closestDistance = distance;
+    }
+  }
+  return closestSlice;
+}
+
+// Does a POW animation at the specified location
+function doPow(x, y) {
+  // In the CSS our animation duration is 200ms
+  // so, when we "setTimeout" we take that into account
+  var smallSizeW = 40;
+  var smallSizeH = 30;
+  var bigSizeW = 260;
+  var bigSizeH = bigSizeW * (smallSizeH/smallSizeW);
+
+  var img = document.createElement("img");
+  img.setAttribute('src', 'img/pow.png');
+  img.classList.add('pow');
+  img.style.width = smallSizeW + "px";
+  img.style.height = smallSizeH + "px";
+  img.style.left = x + "px";
+  img.style.top = y + "px";
+  wrapperEl.appendChild(img);
+
+  // Start growing animation after 200ms
+  setTimeout(function() {
+    img.style.width = bigSizeW + "px";
+    img.style.height = bigSizeH + "px";
+
+    // Adjust ourselves so we would be centered
+    // So we wouldn't grow only to our bottom right
+    img.style.left = (x - ((bigSizeW - smallSizeW) / 2)) + "px";
+    img.style.top = (y - ((bigSizeH - smallSizeH) / 2)) + "px"; 
+
+    // Start fading after the growing animation is over
+    setTimeout(function() {
+      img.style.opacity = "0";
+      
+      // Remove the element after the animation is over
+      setTimeout(function() {
+        img.remove();
+      }, 200);
+
+    }, 600);
+
+  }, 200);
 }
 
 // Create the slices
@@ -93,19 +238,11 @@ for (var i = 0; i < numSlices; i++) {
   sliceEnds[i] = new SliceEnd(i);
 }
 
-function handleSpinButton(event){
-// Set the timer that will update every X ms
-  // event.preventDefault();
-  console.log('this is the beginning of the handler');
-  rotationSpeed = getRandomIntInclusive(5, 100);
-  setInterval(onTimerTick, updateMs);
+for (var i = 1; i < numSlices; i += 2) {
+  sliceEnds[i].turnIntoCat(false);
 }
+//sliceEnds[0].turnIntoGoldenUnicorn();
 
 // Event listener for clicking the spin button
 spinButton.addEventListener('click', handleSpinButton);
-
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
-}
+setInterval(onTimerTick, updateMs);
