c7288955627a01ffbd471d6d3b4e43ea1f7faf2c
surfwalker <alexanderkreid@gmail.com>
2019-05-21 17:04:45 -0700

    Adding array data to localstorage

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -8,16 +8,16 @@ var allPlayersData = JSON.parse(localStorage.getItem('playersStorage')) || [];
 var arrayLength = allPlayersData.length;
 
 // Player constructor function
-function Player(name) {
+function Player(name, points) {
   this.name = name;
-  this.points = [];
+  this.points = points;
 }
 
 function loadFromLocalStorage(dataArray, length) {
-	console.log(length);
-	console.log(dataArray);
+  console.log(length);
+  console.log(dataArray);
   for (var i = 0; i < length; i++) {
-    dataArray.push(new Player(dataArray[i].name));
+    dataArray.push(new Player(dataArray[i].name, dataArray[i].points));
   }
 
   // iteration through array and remove first half
@@ -26,4 +26,61 @@ function loadFromLocalStorage(dataArray, length) {
   }
 }
 
+function makeHeaderRow() {
+
+  // create the row
+  var trEl = document.createElement('tr');
+
+  // create, content, append first cell
+  var thEl = document.createElement('th');
+  thEl.textContent = 'Name';
+  trEl.appendChild(thEl);
+
+  // create, content, append all spin attempts
+  for (var i = 1; i <= 7; i++) {
+    thEl = document.createElement('th');
+    thEl.textContent = "Spin " + i;
+    trEl.appendChild(thEl);
+  }
+
+  // create, content, append total sum of all spins
+  thEl = document.createElement('th');
+  thEl.textContent = 'Total';
+  trEl.appendChild(thEl);
+
+  // append the row to the table
+  leaderboardTable.appendChild(trEl);
+}
+
+var renderLeaderboard = function(array) {
+
+  makeHeaderRow();
+
+  // make a tr
+  var trEl = document.createElement('tr');
+	console.log(array.length);
+  for (var i = 0; i < array.length; i++) {
+
+    // create, content, append for player's name
+    var tdEl = document.createElement('td');
+    tdEl.textContent = array[i].name;
+    trEl.appendChild(tdEl);
+
+		for(var j = 0; j < array[i].points.length; j++) {
+
+    // create, content, append for each hourly total
+    tdEl = document.createElement('td');
+    tdEl.textContent = array[i].points[j];
+		trEl.appendChild(tdEl);
+		}
+  }
+  // // create, content, append for daily total
+  // tdEl = document.createElement('td');
+  // tdEl.textContent = this.totalCookiesPerDay(this.recordOfSalesPerHour);
+  // trEl.appendChild(tdEl);
+  // append the tr to the table
+  leaderboardTable.appendChild(trEl);
+};
+
 loadFromLocalStorage(allPlayersData, arrayLength);
+renderLeaderboard(allPlayersData);
