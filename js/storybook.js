efe08b05cb0dba4c17b407abd9fbfe45ed3bde4d
surfwalker <alexanderkreid@gmail.com>
2019-05-24 09:53:09 -0700

    Audio works

--- js/storybook.js
+++ js/storybook.js
@@ -1,9 +1,9 @@
 'use strict';
 
 // grabbing story flip image DOM elements in storybook.html
-var unicorns = document.getElementById('');
-var laserKitteh = document.getElementById('');
-var goldenUnicorn = document.getElementById('');
+var unicornStory = document.getElementById('unicornstory');
+var laserKittehStory = document.getElementById('laserkittehstory');
+var goldenUnicornStory = document.getElementById('goldenunicornstory');
 
 //Functions to handle sound effects
 function unicornsStoryBook () {
@@ -17,3 +17,8 @@ function laserKittehStoryBook () {
 function goldenUnicornStoryBook () {
 	document.getElementById('golden-unicorn').play();
 }
+
+// event listeners for audio
+unicornStory.addEventListener('click', unicornsStoryBook);
+laserKittehStory.addEventListener('click', laserKittehStoryBook);
+goldenUnicornStory.addEventListener('click', goldenUnicornStoryBook);
